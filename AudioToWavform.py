0a8e33ff034b0fddb53f642212f068a1560ddb49
Dylan Clark <dylan.clark@ideagenplc.com>
2018-08-31 14:57:53 +0100

    got audio extraction and digitisation working

--- AudioToWavform.py
+++ AudioToWavform.py
@@ -1,3 +1,15 @@
-from scipy import wavfile
+# FFMPEG Download
+import imageio
+imageio.plugins.ffmpeg.download()
 
-Fs,data = wavfile.read()
+# MoviePy Audio Extractor
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
+
+# Audio Reader for Numpy Arrays
+from scipy.io.wavfile import read
+
+ffmpeg_extract_audio("C:/work/personaldev/python/amaterasu/files/test.mp4", "C:/work/personaldev/python/amaterasu/files/audio.wav")
+
+rate, data = read("C:/work/personaldev/python/amaterasu/files/audio.wav")
+print(data)
+
