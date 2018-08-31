392824ee92774ae4d61924aab1f854804f4bdb30
Dylan Clark <dylan.clark@ideagenplc.com>
2018-08-31 15:19:47 +0100

    Got it in a numpy array

--- AudioToWavform.py
+++ AudioToWavform.py
@@ -1,15 +1,20 @@
 # FFMPEG Download
 import imageio
 imageio.plugins.ffmpeg.download()
-
 # MoviePy Audio Extractor
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
-
 # Audio Reader for Numpy Arrays
 from scipy.io.wavfile import read
+# MatPlotLib to try and plot the array
+import matplotlib.pyplot as plt
 
 ffmpeg_extract_audio("C:/work/personaldev/python/amaterasu/files/test.mp4", "C:/work/personaldev/python/amaterasu/files/audio.wav")
 
 rate, data = read("C:/work/personaldev/python/amaterasu/files/audio.wav")
 print(data)
+print(type(data))
+
+plt.imshow(data)
+plt.show()
+
 
